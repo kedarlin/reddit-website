3d2caafbee7fe01fde281739a732fe1825d4b136
kedarlin <kedarkamaf@gmail.com>
2024-05-17 13:26:54 +0530

    edited home page and slight changes to atoms and state values

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -19,10 +19,6 @@ type CommunityPageProps = {
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   const setCommunityStateValue = useSetRecoilState(communityState);
 
-  if (!communityData) {
-    return <NotFound />;
-  }
-
   useEffect(() => {
       setCommunityStateValue((prev) => ({
         ...prev,
@@ -31,6 +27,10 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   }, [communityData]);
 
 
+  if (!communityData) {
+    return <NotFound />;
+  }
+  
   return (
     <>
       <Header communityData={communityData} />
